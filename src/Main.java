ed583a7adddcfcf542b491db50f993739dc4168e
Alexandre <alexandre.peron91@gmail.com>
2018-09-19 09:33:48 +0200

    premiere version tp
    simple modif

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	private static Scanner clavier = new Scanner(System.in);
+	
+		public static void main(String[] args) {
+			
+			int aff;
+			System.out.println("Saisir un nombre : ");
+			
+			try{
+				aff = clavier.nextInt();
+				System.out.println(aff);
+			}
+			catch(Exception erreur){
+				System.out.println("Erreur, veuillez saisir un nombre");
+				System.out.println(erreur.toString());
+			}
+			 
+		}
+}
